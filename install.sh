797b0e4fcfe147828fc932e51e5d7728f8099b87
azalus88 <bradleyburgess@gmail.com>
2020-11-04 16:49:36 -0500

    Updated install script

--- install.sh
+++ install.sh
@@ -3,9 +3,17 @@
 
 cd ~
 
-# .bashrc
+# bashrc
 rm .bashrc
 ln -s dotfiles/.bashrc .
+
+# tmuxrc
+rm .tmux.conf
+ln -s dotfiles/.tmux.conf .
+
+# install fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
 
 # vim-plugged
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
