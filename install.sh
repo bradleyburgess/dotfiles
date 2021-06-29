2cf16c290d65fb0543a294250cf43a598601fca3
Bradley Burgess <bradleyburgess@gmail.com>
2021-06-28 22:12:47 -0400

    Cleaned up comments, added i3 symlinks

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@
 
 cd ~
 
-# bashrc
+# symlink bashrc
 mv .bashrc .bashrc_old
 ln -s dotfiles/.bashrc .
 
-# tmuxrc & tpm
+# symlink tmuxrc & install tpm
 mv .tmux.conf .tmux.conf_old
 ln -s dotfiles/.tmux.conf .
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
@@ -16,9 +16,19 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
-# vim-plugged
+# install vim-plugged
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-# nvim
+# symlink nvim
 cd ~/.config
 ln -s ~/dotfiles/nvim .
+
+# symlink i3 configs, if installed
+ITHREE=$(command -v i3)
+if [[ ITHREE != "" ]]
+then
+  mkdir -p ~/.config/i3
+  mkdir -p ~/.config/i3status
+  ln -s ~/dotfiles/i3.conf ~/.config/i3/config
+  ln -s ~/dotfiles/i3status.conf ~/.config/i3status/config
+fi
