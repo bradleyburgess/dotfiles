2b848c9a30c3a6497888c467d7074221d4ec999e
azalus88 <bradleyburgess@gmail.com>
2021-06-30 00:21:10 -0400

    Indenting

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -5,10 +5,10 @@
 # from lukechilds/get_latest_release.sh
 # https://gist.github.com/lukechilds/a83e1d7127b78fef38c2914c4ececc3c
 get_latest_release() {
-  curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
-    grep '"tag_name":' |                                          # Get tag line
-    sed -E 's/.*"([^"]+)".*/\1/'                          # Pluck JSON value
-  }
+  curl --silent "https://api.github.com/repos/$1/releases/latest" |
+    grep '"tag_name":' |
+    sed -E 's/.*"([^"]+)".*/\1/'
+}
 
 # First run check for required packages:
 echo -e "\nChecking for Neovim..."
